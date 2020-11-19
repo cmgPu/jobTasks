c2b21d70d90b062e450cb4863c344f381e8c2e8e
ammar <ammar.botonjic@convergent-usa.com>
2020-11-19 09:11:13 +0100

    Implementation of task 10

--- Task10 - LanguageEncoder - Javascript/app.js
+++ Task10 - LanguageEncoder - Javascript/app.js
@@ -1,6 +1,15 @@
 function encode(string) {
     // YOUR IMPLEMENTATION...
-    return '';
+    let convertedStringToArray = string.split('');
+    let vowels = 'aeiouAEIOU'.split('');
+
+    for (let index = 0; index < convertedStringToArray.length; index++) {
+        if (vowels.includes(convertedStringToArray[index])) {
+            convertedStringToArray[index] += 'p' + convertedStringToArray[index].toLowerCase();
+        }
+    }
+
+    return convertedStringToArray.join('');
 };
 
 module.exports = { encode };
